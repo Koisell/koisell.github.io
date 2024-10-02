464a4c7f39df3db3db8b9bdb32780891ed255e78
sampadbarman <52348316+sampadbarman@users.noreply.github.com>
2024-10-02 15:55:34 +0530

    index

--- count-down.js
+++ count-down.js
@@ -1,7 +1,11 @@
 $(document).on("click", function () {
   document.getElementById("my_audio").play();
-  console.log("Shaadi me zaroor aana");
 });
+
+(function ($) {
+  "use strict";
+  $(".sakura-falling").sakura();
+})(jQuery);
 
 // Set the date we're counting down to
 var countDownDate = new Date("11-25-2024").getTime();
@@ -35,40 +39,3 @@ var x = setInterval(function () {
     document.getElementById("seconds").innerHTML = "EXPIRED";
   }
 }, 1000);
-
-// // Setting the Launch Date.
-// var comingdate = new Date("November 25, 2024 00:00:00");
-
-// // Accepting the elements.
-// var d = document.getElementById("d");
-// var h = document.getElementById("h");
-// var m = document.getElementById("m");
-// var s = document.getElementById("s");
-
-// // Making Countdown work.
-// var x = setInterval(function () {
-//   var now = new Date();
-//   var des = comingdate.getTime() - now.getTime();
-//   var days = Math.floor(des / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((des % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var mins = Math.floor((des % (1000 * 60 * 60)) / (1000 * 60));
-//   var secs = Math.floor((des % (1000 * 60)) / 1000);
-
-//   d.innerHTML = getTrueNumber(days);
-//   h.innerHTML = getTrueNumber(hours);
-//   m.innerHTML = getTrueNumber(mins);
-//   s.innerHTML = getTrueNumber(secs);
-
-//   if (des <= 0) {
-//     clearInterval(x);
-//     d.innerHTML = "00";
-//     h.innerHTML = "00";
-//     m.innerHTML = "00";
-//     s.innerHTML = "00";
-//   }
-// }, 1000);
-
-// function getTrueNumber(x) {
-//   if (x < 10) return "0" + x;
-//   else return x;
-// }
